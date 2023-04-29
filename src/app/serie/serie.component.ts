2887ba4b426126cf00db1bb3cddb4637ce48643e
Jeronimo <88887550+Jeronimo2122@users.noreply.github.com>
2023-04-29 01:19:15 -0500

    TALLER

---
+++ src/app/serie/serie.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { dataseries } from './dataSeries';
+import { SerieService } from './serie.service';
+
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+  temporadas: number = 0;
+  promedio: number = 0;
+
+  constructor(private serieService: SerieService) { }
+  series: Array<Serie> = [];
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(series => {
+      this.series = series;
+      console.log(series);
+    });
+    this.getPromedioTemporadas();
+  }
+
+  // Aqui va el calculo de los promedios
+
+  getPromedioTemporadas(){
+    this.serieService.getSeries().subscribe(series=>{
+      series.forEach(serie => this.temporadas+=serie.seasons);
+      this.promedio=this.temporadas/series.length;
+    });
+    console.log(`Average ${this.promedio} , Seasons ${this.temporadas}`)
+
+  }
+
+
+  ngOnInit() {
+    this.getSeries();
+  }
+
+}
+
